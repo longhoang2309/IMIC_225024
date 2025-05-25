9061e6eac911d97bcee84c380d8e869cd7795b70
longhoang2309 <lhoangbris@gmail.com>
2025-05-25 09:47:00 +0700

    bài 6 xếp loại học sinh

--- Main.c
+++ Main.c
@@ -1,4 +1,20 @@
+﻿
 #include<stdio.h>
 void main() {
-	printf("Hello world\n");
+	unsigned int local = 0;
+	printf("nhap diem: ");
+	scanf_s("%d", &local);
+	if (local >= 8) {
+		printf("Gioi");
+	}
+	else if(8> local >= 6.5 ){
+		printf("Kha");
+	}
+	else if (6.5 > local >= 5) {
+		printf("Trung Binh");
+	}
+	else{
+		printf("Yeu");
+	}
+	
 }
