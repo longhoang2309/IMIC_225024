d6e68f4489f1a889c85743215a9efbbd75f000bd
longhoang2309 <lhoangbris@gmail.com>
2025-08-29 20:25:27 +0700

    8:25

--- Main.cpp
+++ Main.cpp
@@ -1,8 +1,6 @@
 ﻿#include<stdio.h>
 #include<iostream>
-#include<malloc.h>
-#include<array>
-#include<string.h>
+#include<functional>
 #include<vector>
 using namespace std;
 struct hoc_sinh
@@ -12,22 +10,9 @@ struct hoc_sinh
 	float diem_van;
 };
 
-bool diemtb(hoc_sinh& a)
-{
-	return ((a.diem_toan + a.diem_van) / 2);
-}
 
-//viet ham so sanh 2 hs dua vao diem tb
-bool sosanh_diemtb(hoc_sinh &a, hoc_sinh &b)
-{
-	if (diemtb(a) > diemtb(b))
-		return true;
-	else
-		return false;
-}
-
-template<typename kieu_du_lieu>
-void sap_xep_mang(vector<kieu_du_lieu> &a, bool(*pf)(kieu_du_lieu&, kieu_du_lieu&))
+template<typename kieu_du_lieu, typename lambda_t>
+void sap_xep_mang(vector<kieu_du_lieu> &a, lambda_t ham_so_sanh)
 {
 	for (int i = 0; i < a.size() - 1; i++)
 	{
@@ -36,21 +21,32 @@ void sap_xep_mang(vector<kieu_du_lieu> &a, bool(*pf)(kieu_du_lieu&, kieu_du_lieu
 			if (fp(a[i], a[j]))
 			{
 				kieu_du_lieu tmp = a[j];
-				a[i] = a[j];
+				a[j] = a[i];
 				a[j] = tmp;
 			}
 		}
 	}
 }
+
+
 int main()
 {
 	vector<int> vec= { 5,3,56,781,1 };
+	sap_xep_mang(vec, [](int a, int b) {
+		return a > b;
+		});
+
 	vector<hoc_sinh> vec2 = {
 		{"ngyen van a",8.1,8.2},
 		{"ngyen van b",1,8.1},
 		{"ngyen van c",8.8,2},
 		{"ngyen van d",7.1,7.2}
 	};
-	sap_xep_mang(vec2, sosanh_diemtb);
+	sap_xep_mang(vec2, [](hoc_sinh a, hoc_sinh b) {
+		float a_tb = (a.diem_toan + a.diem_van) / 2;
+		float b_tb = (b.diem_toan + b.diem_van) / 2;
+		return a_tb > b_tb;
+		});
+	//sap_xep_mang(vec2, sosanh_diemtb);
 	return 0;
 }
